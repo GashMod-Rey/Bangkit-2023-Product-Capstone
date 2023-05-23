92c2e1ddb69a45f3b0ce00876506a515357730b1
Nadya Angelia <nadya.angel126@gmail.com>
2023-05-23 17:11:46 +0100

    [MD - Naya] Intent for Applicants

--- mobile/app/src/main/java/umn/ac/id/myapplication/ui/applicantpage/LoginActivity.kt
+++ mobile/app/src/main/java/umn/ac/id/myapplication/ui/applicantpage/LoginActivity.kt
@@ -1,12 +1,27 @@
 package umn.ac.id.myapplication.ui.applicantpage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import umn.ac.id.myapplication.R
+import umn.ac.id.myapplication.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        binding.buttonLogin.setOnClickListener {
+            Intent(this, MainActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        binding.Signup.setOnClickListener {
+            Intent(this@LoginActivity, SignUpActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
--- mobile/app/src/main/java/umn/ac/id/myapplication/ui/applicantpage/SignUpActivity.kt
+++ mobile/app/src/main/java/umn/ac/id/myapplication/ui/applicantpage/SignUpActivity.kt
@@ -1,13 +1,27 @@
 package umn.ac.id.myapplication.ui.applicantpage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import umn.ac.id.myapplication.R
+import umn.ac.id.myapplication.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
-
+private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding.root)
+
+        binding.login.setOnClickListener{
+            Intent(this@SignUpActivity, LoginActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        binding.buttonSignup.setOnClickListener {
+            Intent(this@SignUpActivity, LoginActivity::class.java).also{
+                startActivity(it)
+            }
+        }
     }
 }
