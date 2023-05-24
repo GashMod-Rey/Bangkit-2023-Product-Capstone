c06e138f32819b3b3bcfb998a686f1bd69ba659f
Nadya Angelia <nadya.angel126@gmail.com>
2023-05-24 17:38:42 +0100

    [MD - Naya] Intent for Applicants, Fixing some Layout

--- mobile/app/src/main/java/umn/ac/id/myapplication/ui/applicantpage/UploadCvSuccessActivity.kt
+++ mobile/app/src/main/java/umn/ac/id/myapplication/ui/applicantpage/UploadCvSuccessActivity.kt
@@ -1,12 +1,23 @@
 package umn.ac.id.myapplication.ui.applicantpage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import umn.ac.id.myapplication.R
+import umn.ac.id.myapplication.databinding.ActivityUploadCvSuccessBinding
+import umn.ac.id.myapplication.ui.applicantpage.ui.profile.ProfileFragment
 
 class UploadCvSuccessActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUploadCvSuccessBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upload_cv_success)
+        binding = ActivityUploadCvSuccessBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonDone.setOnClickListener {
+            Intent(this, ProfileFragment::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
--- mobile/app/src/main/java/umn/ac/id/myapplication/ui/applicantpage/ui/history/HistoryFragment.kt
+++ mobile/app/src/main/java/umn/ac/id/myapplication/ui/applicantpage/ui/history/HistoryFragment.kt
@@ -1,5 +1,6 @@
 package umn.ac.id.myapplication.ui.applicantpage.ui.history
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,9 +8,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import umn.ac.id.myapplication.R
+import umn.ac.id.myapplication.databinding.FragmentHistoryBinding
+import umn.ac.id.myapplication.databinding.FragmentProfileBinding
+import umn.ac.id.myapplication.ui.applicantpage.HistoryRejectActivity
+import umn.ac.id.myapplication.ui.applicantpage.HistorySuccessActivity
 
 class HistoryFragment : Fragment() {
 
+    private var _binding: FragmentHistoryBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
     companion object {
         fun newInstance() = HistoryFragment()
     }
@@ -20,7 +30,23 @@ class HistoryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        binding.buttonSuccess.setOnClickListener {
+            Intent(requireContext(), HistorySuccessActivity::class.java).also{
+                startActivity(it)
+            }
+        }
+        binding.buttonReject.setOnClickListener {
+            Intent(requireContext(), HistoryRejectActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+        return root
+
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
