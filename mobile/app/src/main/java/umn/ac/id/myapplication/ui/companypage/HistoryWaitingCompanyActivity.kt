102ae9895cd4dc0f9361a4e4af1115fe65ec2d1c
gianzra <gianzra1@gmail.com>
2023-05-27 13:50:54 +0700

    [MD - Gian] Intent for Company Finish

--- mobile/app/src/main/java/umn/ac/id/myapplication/ui/companypage/HistoryWaitingCompanyActivity.kt
+++ mobile/app/src/main/java/umn/ac/id/myapplication/ui/companypage/HistoryWaitingCompanyActivity.kt
@@ -3,10 +3,15 @@ package umn.ac.id.myapplication.ui.companypage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import umn.ac.id.myapplication.R
+import umn.ac.id.myapplication.databinding.ActivityHistoryRejectBinding
+import umn.ac.id.myapplication.databinding.ActivityHistoryWaitingCompanyBinding
+import umn.ac.id.myapplication.databinding.ActivityWaitingHiredCompanyBinding
 
 class HistoryWaitingCompanyActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWaitingHiredCompanyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_history_waiting_company)
+        binding = ActivityWaitingHiredCompanyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- mobile/app/src/main/java/umn/ac/id/myapplication/ui/companypage/ui/HistoryCompany/HistoryCompanyFragment.kt
+++ mobile/app/src/main/java/umn/ac/id/myapplication/ui/companypage/ui/HistoryCompany/HistoryCompanyFragment.kt
@@ -1,5 +1,6 @@
 package umn.ac.id.myapplication.ui.companypage.ui.HistoryCompany
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,15 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import umn.ac.id.myapplication.databinding.FragmentHistoryBinding
 import umn.ac.id.myapplication.databinding.FragmentHistoryCompanyBinding
+import umn.ac.id.myapplication.ui.applicantpage.HistoryRejectActivity
+import umn.ac.id.myapplication.ui.applicantpage.HistorySuccessActivity
+import umn.ac.id.myapplication.ui.applicantpage.ui.history.HistoryFragment
+import umn.ac.id.myapplication.ui.applicantpage.ui.history.HistoryViewModel
+import umn.ac.id.myapplication.ui.companypage.HistoryDeclineCompanyActivity
+import umn.ac.id.myapplication.ui.companypage.HistorySuccessCompanyActivity
+import umn.ac.id.myapplication.ui.companypage.HistoryWaitingCompanyActivity
 
 class HistoryCompanyFragment : Fragment() {
 
@@ -16,27 +25,49 @@ class HistoryCompanyFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    companion object {
+        fun newInstance() = HistoryFragment()
+    }
+
+    private lateinit var viewModel: HistoryViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val historyViewModel =
-            ViewModelProvider(this).get(HistoryCompanyViewModel::class.java)
-
+    ): View? {
         _binding = FragmentHistoryCompanyBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textView
-        historyViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+
+        binding.buttonSuccess.setOnClickListener {
+            Intent(requireContext(), HistorySuccessCompanyActivity::class.java).also{
+                startActivity(it)
+            }
+        }
+        binding.buttonReject.setOnClickListener {
+            Intent(requireContext(), HistoryDeclineCompanyActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+        binding.buttonWaiting.setOnClickListener {
+            Intent(requireContext(), HistoryWaitingCompanyActivity::class.java).also {
+                startActivity(it)
+            }
         }
         return root
+
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(HistoryViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
 }
