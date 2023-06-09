91e78449969fc8dfd858ce3598e04742ea3cd424
GashMod-Rey <reynard.yaputra@student.umn.ac.id>
2023-06-09 19:26:34 +0700

    [Rey - ML] Scoring Update

--- script/Scoring.py
+++ script/Scoring.py
@@ -1,4 +1,53 @@
 import numpy as np
+import sys
+
+def score_skill(skill, skillFilter):
+    skill = [x.upper() for x in skill]
+    skillFilter = [x.upper() for x in skillFilter]
+
+    skill = set(skill).intersection(skillFilter)
+
+    intersection = len(list(set(skill).intersection(skillFilter)))
+    union = (len(set(skill)) + len(set(skillFilter))) - intersection
+
+    jaccard_index = float(intersection) / union
+    return round(jaccard_index, 2)
+
+def score_lang(lang, langFilter):
+    lang = [x.upper() for x in lang]
+    langFilter = [x.upper() for x in langFilter]
+
+    lang = set(lang).intersection(langFilter)
+
+    intersection = len(list(set(lang).intersection(langFilter)))
+    union = (len(set(lang)) + len(set(langFilter))) - intersection
+
+    jaccard_index = float(intersection) / union
+    return round(jaccard_index, 2)
+
+def score_age(age, ageFilter, tolerance):
+    if age >= ageFilter[0] and age <= ageFilter[1]:
+        index = 1
+    elif age >= ageFilter[1] and age <= ageFilter[1] + tolerance:
+        index = round((tolerance + 1 - age + ageFilter[1])/(tolerance + 1), 2)
+    elif age <= ageFilter[0] and age >= ageFilter[0] - tolerance:
+        index = round((tolerance + 1 - ageFilter[0] + age)/(tolerance + 1), 2)
+    else:
+        index = 0
+
+    return index
+
+def score_salary(salary, salaryFilter, tol):
+    if salary >= salaryFilter[0] and salary <= salaryFilter[1]:
+        index = 1
+    elif salary >= salaryFilter[1]:
+        index = round((tol + 1 + salaryFilter[1] - salary)/(tol + 1), 2)
+        if index < 0:
+            index = 0
+    elif salary <= salaryFilter[0]:
+        index = 1
+
+    return index
 
 def scoring(data_appl, data_filter):
     x = []
@@ -17,4 +66,8 @@ def scoring(data_appl, data_filter):
         i = i+1
 
     x = np.array(x).reshape(-1, 4)
-    return x
+    print(x)
+
+data_appl = sys.argv[1]
+data_filter = sys.argv[2]
+scoring(data_appl, data_filter)
