487d684a80795796490b6ab14e09c2aab8fb8548
gianzra <gianzra1@gmail.com>
2023-06-10 12:02:26 +0700

    [ MD - Gian ] Add log out feature

--- mobile/app/src/main/java/umn/ac/id/myapplication/ui/applicantpage/SettingsActivity.kt
+++ mobile/app/src/main/java/umn/ac/id/myapplication/ui/applicantpage/SettingsActivity.kt
@@ -1,5 +1,6 @@
 package umn.ac.id.myapplication.ui.applicantpage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -12,13 +13,22 @@ import umn.ac.id.myapplication.ui.applicantpage.ui.home.HomeFragment
 import umn.ac.id.myapplication.ui.applicantpage.ui.profile.ProfileFragment
 
 class SettingsActivity : AppCompatActivity() {
-  private lateinit var binding: ActivitySettingsBinding
-  override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var binding: ActivitySettingsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.buttonAboutme.setOnClickListener {
+            performLogout()
+        }
+    }
 
+    private fun performLogout() {
 
- }
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 }
