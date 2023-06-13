e9d4e1c111c2cd5c522318fde3b0eb177d9086f0
nehemiagueldi <nehemia.gueldi@student.umn.ac.id>
2023-06-13 23:22:25 +0700

    [Nehe - CC] Testing di Android Studio

--- mobile/app/src/main/java/umn/ac/id/myapplication/ui/api/ApiClient.kt
+++ mobile/app/src/main/java/umn/ac/id/myapplication/ui/api/ApiClient.kt
@@ -4,7 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-private const val BASEURL = "http://192.168.100.12:8000/" //GianURL
+//private const val BASEURL = "http://192.168.100.12:8000/" //GianURL
+private const val BASEURL = "http://192.168.18.9:8000/" //NeheURL
 //    private const val BASEURL = "http://192.168.43.20:8000/"
 //private const val BASEURL = "https://backend-dot-capstone-project-hirehub.et.r.appspot.com/"
     val retrofit = Retrofit.Builder()
